8ff9740998e4986d326407156cd7b3280acefb9f
GabrielGRSilva <gabrielgrsilva@outlook.com.br>
2025-05-21 14:04:25 -0300

    We've got a rotating triangle... I mean, spaceship!

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
-import pygame
+
+import pygame     #opensource Python library for games
+from player import *
 from constants import *
 
 def main():
@@ -9,16 +8,24 @@ def main():
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    pygame.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        
-    while True:
-        pygame.Surface.fill(pygame.screen, "black")
-        pygame.display.flip()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    fpsclock = pygame.time.Clock()
+    dt = 0
 
-        for event in pygame.event.get():
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
+        
+    while True:                                     #Game Loop
+        for event in pygame.event.get():            #For Loop to make the quit "X" work
             if event.type == pygame.QUIT:
               return
+            
+        screen.fill("black")
+        player.draw(screen)
+        player.update(dt)                   #update the player's position/rotation   
+        pygame.display.flip()               #Update the full display Surface to the screen
+        
 
+        dt = (fpsclock.tick(60)) /1000                   ##This limits the FPS to 60
 
 if __name__ == "__main__":
     main()
